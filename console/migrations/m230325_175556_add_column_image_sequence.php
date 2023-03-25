82b38413ed8cf4a7ecf15d1703b93115dfae72fa
Lancer Pavel <finecopter@advertising-page.com>
2023-03-25 14:04:28 -0400

    added the sequence column

---
+++ console/migrations/m230325_175556_add_column_image_sequence.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230325_175556_add_column_image_sequence
+ */
+class m230325_175556_add_column_image_sequence extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+		$this->addColumn('{{%car_image}}', 'sequence', $this->boolean()->defaultValue(0));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%car_image}}', 'sequence');
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230325_175556_add_column_image_sequence cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
